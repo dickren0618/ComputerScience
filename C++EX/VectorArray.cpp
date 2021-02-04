e76ca424baa9cc9b1a8ada35c28d83e7597a5830
dickren0618 <66706198+dickren0618@users.noreply.github.com>
2021-02-04 17:08:41 +0800

    Add files via upload

---
+++ C++EX/VectorArray.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+double average(const vector<double>& arr)
+{
+	double sum = 0;
+	for (unsigned i = 0; i < arr.size(); i++)
+		sum += arr[i];
+	return sum / arr.size();
+}
+void main()
+{
+	vector<double> arr(4);
+	for (int i = 0; i < 4; i++)
+		arr[i] = 4;
+	cout<<average(arr)<<endl;
+	
+}
+
